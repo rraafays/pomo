292bdbc1eb922d5d1f88b83d7bb203a51d88bf9b
rraafays <rraf@tuta.io>
2022-09-23 18:41:53 +0100

    update: created basic timer

--- pomo.c
+++ pomo.c
@@ -3,11 +3,16 @@
 
 int main(int argc, char *argv[])
 {
-  for (int i = 0; i < 10; i++)
+  for (int m = 1; m >= 0; m--)
   {
-    printf("\r%d", i + 1);
-    sleep(1);
-    fflush(stdout);
+    int s = 0;
+    for (s = 59; s >= 0; s--)
+    {
+      sleep(1);
+      printf("\r%02d:%02d", m, s);
+      fflush(stdout);
+    }
+    s = 59;
   }
 
   return 0;
