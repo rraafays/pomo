f99ca8011f0b622ba759537547b3fabca1e614d3
rraafays <rraf@tuta.io>
2022-09-23 18:15:58 +0100

    chore: timed 1 - 10 test

--- pomo.c
+++ pomo.c
@@ -3,9 +3,12 @@
 
 int main(int argc, char *argv[])
 {
-  printf("Hello");
-  fflush(stdout);
-  sleep(1);
-  printf("\rWorld!");
+  for (int i = 0; i < 10; i++)
+  {
+    printf("\r%d", i + 1);
+    sleep(1);
+    fflush(stdout);
+  }
+
   return 0;
 }
