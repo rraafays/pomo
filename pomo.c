8f32a8f748c712bad4a4aad4deedd41546e64d17
rraafays <rraf@tuta.io>
2022-09-24 18:55:09 +0100

    update: finally got timers to start at whole minutes with 0 seconds

--- pomo.c
+++ pomo.c
@@ -6,26 +6,26 @@ int main(int argc, char *argv[]) // proper version of int main() which includes 
   int s = 0; // declares an empty integer used as seconds
   while (1) // infinite while loop
   {
-    for (int m = 24; m >= 0; m--) // 2d for loop to deduct minutes every 60 seconds
+    for (int m = 25; m >= 0; m--) // 2d for loop to deduct minutes every 60 seconds
     {
-      for (s = 59; s >= 0; s--) // 60 second timer
+      for (s = 0; s >= 0; s--) // 60 second timer
       {
         printf("\r%02d:%02d 🍅 Work time!", m, s);
         fflush(stdout);
+        if (s == 0) { m--; s = 60; }
         sleep(1);
       }
-      s = 59; // resets seconds every complete cycle
     }
 
-    for (int m = 4; m >= 0; m--)
+    for (int m = 5; m >= 0; m--)
     {
-      for (s = 59; s >= 0; s--)
+      for (s = 60; s >= 0; s--)
       {
         printf("\r%02d:%02d 🍵 Break time grab some tea! ", m, s);
         fflush(stdout);
+        if (s == 0) { m--; s = 60; }
         sleep(1);
       }
-      s = 59;
     }
   }
 
