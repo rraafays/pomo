815b7591508bd223910098fee128d4ded142497d
raf <rraf@tuta.io>
2022-09-27 09:08:33 +0100

    chore: fixed bug where pomo would break time and space

--- pomo.c
+++ pomo.c
@@ -12,19 +12,21 @@ int main(int argc, char *argv[]) // proper version of int main() which includes 
       {
         printf("\r%02d:%02d 🍅 Work time!", m, s);
         fflush(stdout); // cleans the output
-        if (s == 0) { m--; s = 60; } // if we hit 0 seconds then it deducts a minute and resets to 60
         sleep(1); // sleeps for 1 second
+        if (m == 0 && s == 0) { break; }
+        if (s == 0) { m--; s = 60; } // if we hit 0 seconds then it deducts a minute and resets to 60
       }
     }
 
     for (int m = 5; m >= 0; m--)
     {
-      for (s = 60; s >= 0; s--)
+      for (s = 0; s >= 0; s--)
       {
         printf("\r%02d:%02d 🍵 Break time grab some tea! ", m, s);
         fflush(stdout);
-        if (s == 0) { m--; s = 60; }
         sleep(1);
+        if (m == 0 && s == 0) { break; }
+        if (s == 0) { m--; s = 60; }
       }
     }
   }
