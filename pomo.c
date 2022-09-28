790c3712e835dbd824ff2f381776dcf537eaa42c
rraafays <rraf@tuta.io>
2022-09-28 10:25:04 +0100

    update: created a separate timer method which is called by the main method

--- pomo.c
+++ pomo.c
@@ -1,35 +1,32 @@
 #include <stdio.h> // includes standard input output header file for printf function
 #include <unistd.h> // includes unix standard header file for sleep function
+#include <string.h> // includes standard string library for strcmp function
+
+void timer(int m, char t[]);
 
 int main(int argc, char *argv[]) // proper version of int main() which includes args and arg counter
 {
-  int s = 0; // declares an empty integer used as seconds
   while (1) // infinite while loop
   {
-    for (int m = 25; m >= 0; m--) // 2d for loop to deduct minutes every 60 seconds
-    {
-      for (s = 0; s >= 0; s--) // 60 second timer
-      {
-        printf("\r%02d:%02d 🍅 Work time!", m, s);
-        fflush(stdout); // cleans the output
-        sleep(1); // sleeps for 1 second
-        if (m == 0 && s == 0) { break; }
-        if (s == 0) { m--; s = 60; } // if we hit 0 seconds then it deducts a minute and resets to 60
-      }
-    }
+    timer(25, "🍅 Work time!");
+    timer(5, "🍵 Break time, grab some tea");
+  }
+  return 0; // gracefully exits program should while loop break
+}
 
-    for (int m = 5; m >= 0; m--)
+void timer(int m, char t[]) // function which takes in a number of minutes represented by 'm' and also takes in a string represented by 't'
+{
+  int s = 0; // declares an empty integer used as seconds
+  for (int x = m; x >= 0; x--) // 2d for loop to deduct minutes every 60 seconds
+  {
+    for (s = 0; s >= 0; s--) // 60 second timer
     {
-      for (s = 0; s >= 0; s--)
-      {
-        printf("\r%02d:%02d 🍵 Break time grab some tea! ", m, s);
-        fflush(stdout);
-        sleep(1);
-        if (m == 0 && s == 0) { break; }
-        if (s == 0) { m--; s = 60; }
-      }
+      printf("\33[2K\r"); // clears the current line
+      printf("%02d:%02d %s", x, s, t);
+      fflush(stdout); // cleans the output
+      sleep(1); // sleeps for 1 second
+      if (x == 0 && s == 0) { break; }
+      if (s == 0) { x--; s = 60; } // if we hit 0 seconds then it deducts a minute and resets to 60
     }
   }
-
-  return 0; // gracefully exits program should while loop break
 }
